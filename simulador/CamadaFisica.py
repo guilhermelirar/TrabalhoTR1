07f7c01243428276e6c4bdb87b6f1ddc3675864c
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-05-12 10:41:57 -0300

    usando numpy e plotando

--- simulador/CamadaFisica.py
+++ simulador/CamadaFisica.py
@@ -1,10 +1,11 @@
+import numpy as np
+
 def modularNRZ_Polar(bitstream: list[int], volt_high: float = 5.0, 
                      volt_low: float = -5.0, 
-                     amostras_p_bit: int = 100) -> list[float]:
-    valores = [volt_high if b else volt_low for b in bitstream]
-    amostras = []
+                     amostras_p_bit: int = 100) -> np.ndarray:
 
-    for v in valores:
-        amostras.extend([v]*amostras_p_bit)
+    bits = np.array(bitstream)
+    valores = np.where(bits > 0, volt_high, volt_low)
+    amostras = np.repeat(valores, amostras_p_bit)
 
     return amostras
--- simulador/Simulador.py
+++ simulador/Simulador.py
@@ -1,5 +1,28 @@
-def main():
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
 
-if __name__ == "__mai__":
+from CamadaFisica import modularNRZ_Polar
+
+def plot(sinal: np.ndarray, amostras_p_bit: int) -> None:
+    plt.plot(np.arange(len(sinal)), sinal)
+    plt.grid(True)
+
+    i = 0
+    while i * amostras_p_bit <= len(sinal):
+        plt.axvline(i * amostras_p_bit, color='r', linestyle=':')
+        i += 1
+
+    plt.ylabel("Tensão (V)")
+    plt.xlabel("Amostras")
+    plt.show()
+
+
+def main():
+    info = [1, 1, 0, 1]
+    plot(modularNRZ_Polar(info), 100)
+
+    pass
+    
+
+if __name__ == "__main__":
     main()
