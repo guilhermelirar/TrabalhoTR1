4da1c392d54adffd25aa265c73bd7720573786df
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-06-25 07:57:28 -0300

    Utils

--- simulador/rx/CamadaEnlace.py
+++ simulador/rx/CamadaEnlace.py
@@ -1,37 +1,4 @@
-def bitstream_to_int(bits: list[int]) -> int:
-    """Função auxiliar para converter uma 
-    lista de bits de volta para inteiro"""
-    bin_str = "".join(map(str, bits))
-    return int(bin_str, 2)
-
-def str_to_bitstream(string: str):
-    res_str = ''.join(format(ord(char), '08b') for char in string)
-    return [int(c) for c in res_str]
-
-def int_to_bitstream(number: int, max_bytes: int) -> list[int]:
-    total_bits = max_bytes * 8
-    binary_str = format(number, f'0{total_bits}b')
-    return [int(bit) for bit in binary_str]
-
-def bits_para_hexa(lista_de_bits):
-    # Agrupa a lista de 8 em 8 bits
-    bytes_list = [lista_de_bits[i:i+8] 
-                  for i in range(0,len(lista_de_bits), 8)]
-    
-    hexa_str = []
-    for b in bytes_list:
-        # numero inteiro
-        bin_str = "".join(map(str, b)) # string binária
-        num = int(bin_str, 2)          # inteiro do binário
-
-        if len(b) == 8:
-            # hexa decimal
-            hexa_str.append(f"{num:02X}h")
-        
-        else:
-            hexa_str.append(f"{num:07b}b")
-
-    return " ".join(hexa_str)
+from Utils import *
 
 # === DESENQUADRAMENTO === #
 
--- simulador/tx/CamadaEnlace.py
+++ simulador/tx/CamadaEnlace.py
@@ -1,39 +1,11 @@
 # simulador/tx/CamadaEnlace.py
 
+from Utils import *
+
 """
 Implementação dos serviços da camada da camada de 
 enlace
 """
-
-def str_to_bitstream(string: str):
-    res_str = ''.join(format(ord(char), '08b') for char in string)
-    return [int(c) for c in res_str]
-
-def int_to_bitstream(number: int, max_bytes: int) -> list[int]:
-    total_bits = max_bytes * 8
-    binary_str = format(number, f'0{total_bits}b')
-    return [int(bit) for bit in binary_str]
-
-def bits_para_hexa(lista_de_bits):
-    # Agrupa a lista de 8 em 8 bits
-    bytes_list = [lista_de_bits[i:i+8] 
-                  for i in range(0,len(lista_de_bits), 8)]
-    
-    hexa_str = []
-    for b in bytes_list:
-        # numero inteiro
-        bin_str = "".join(map(str, b)) # string binária
-        num = int(bin_str, 2)          # inteiro do binário
-
-        if len(b) == 8:
-            # hexa decimal
-            hexa_str.append(f"{num:02X}h")
-        
-        else:
-            hexa_str.append(f"{num}d")
-
-    return " ".join(hexa_str)
-
 
 # === 1/3 Protocolos de enquadramento === #
 
